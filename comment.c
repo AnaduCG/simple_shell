0437ac103b7e0ce52028191c46c5f214da9fc62b
AnaduCG <godzionwin@gmail.com>
2023-05-25 20:44:15 +0100

    Update comment feature algorithm to be more accurate

--- comment.c
+++ comment.c
@@ -4,7 +4,7 @@
  *		the user input
  *@input: user input to be checked
  *@e_ex: the program executable for proper error display
- *@sc: session count for proper error disply
+ *@sc: session count for proper error display
  *@_environ: custom environmental variables ofr the program
  *Return: returns nonzero on error and zero on success
  */
@@ -14,10 +14,7 @@ int comment(char *input, char *e_ex, int sc, list_t *_environ)
 
 	if (input[0] == '#')
 	{
-		if (input[1])
-			input[1] = '\0';
-		else
-			input[0] = ' ';
+		input[0] = '\0';
 	}
 	for (i = 0; input[i]; i++)
 	{
